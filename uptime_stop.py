a814c15e85c74da2aef97c2cc5e1b2aad9ad039d
thimo-wellner <74245455+thimo-wellner@users.noreply.github.com>
2022-01-30 16:52:39 +0100

    Update uptime_stop.py

--- uptime_stop.py
+++ uptime_stop.py
@@ -8,19 +8,28 @@ import json
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-time = 10*60*60
+hours = 10
+time = hours*60*60
+idle = False
+
+def get_notebook_name():
+    log_path = '/opt/ml/metadata/resource-metadata.json'
+    with open(log_path, 'r') as logs:
+        _logs = json.load(logs)
+    return _logs['ResourceName']
 
 client = boto3.client('sagemaker')
 uptime = client.describe_notebook_instance(
-  NotebookInstanceName=get_notebook_name()
-  )['LastModifiedTime']
-starting_time = datetime.strptime(uptime,"%Y-%m-%dT%H:%M:%S.%fz")
-    if (datetime.now() - starting_time).total_seconds() > time:
-      idle = True
+    NotebookInstanceName=get_notebook_name()
+    )['LastModifiedTime']
+starting_time = datetime.strptime(uptime.strftime("%Y-%m-%dT%H:%M:%S.%fz"),"%Y-%m-%dT%H:%M:%S.%fz")
+if (datetime.now() - starting_time).total_seconds() > time:
+    idle = True
       
 if idle:
-    print('Closing idle notebook')
+    print(f'Closing notebook after {hours} hours of uptime!')
     client = boto3.client('sagemaker')
     client.stop_notebook_instance(
-        NotebookInstanceName=get_notebook_name()
-    )
+        NotebookInstanceName=get_notebook_name())
+else:
+    print("Not yet time to close")
